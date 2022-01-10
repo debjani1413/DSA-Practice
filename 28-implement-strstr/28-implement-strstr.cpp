ec12fb0736b6227a03ec2a4fad198d2464f13d29
debjani1413 <65902742+debjani1413@users.noreply.github.com>
2022-01-10 21:57:05 +0530

    Time: 20 ms (60.29%), Space: 7.2 MB (63.34%) - LeetHub

---
+++ 28-implement-strstr/28-implement-strstr.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int strStr(string haystack, string needle) {
+        if(needle.empty())
+            return 0;
+       if(haystack.find(needle)!= string::npos)
+           return haystack.find(needle);
+        else
+            return -1;
+        return 0;
+        }
+};
