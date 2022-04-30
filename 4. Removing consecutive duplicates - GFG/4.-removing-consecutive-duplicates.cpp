ea07b50fecec449ed6fb2d3fc0d12a96cdc784ee
debjani1413 <65902742+debjani1413@users.noreply.github.com>
2022-04-30 12:45:33 +0530

    Added solution - LeetHub

---
+++ 4. Removing consecutive duplicates - GFG/4.-removing-consecutive-duplicates.cpp
@@ -0,0 +1,41 @@
+// { Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+
+
+class Solution
+{
+    public:
+    //Function to remove consecutive duplicates from given string using Stack.
+    string removeConsecutiveDuplicates(string s)
+    {
+        // Your code here
+        string t="";
+        for(int i=0;i<s.length();i++)
+        {
+            if(s[i]!=s[i+1])
+            t=t+s[i];
+        }
+        return t;
+    }
+};
+
+// { Driver Code Starts.
+
+int main() {
+    int t;
+    cin>>t;
+    
+    while(t--)
+    {
+        string s;
+        cin>>s;
+        Solution obj;
+        cout<<obj.removeConsecutiveDuplicates(s)<<endl;
+    }
+    
+	return 0;
+}  // } Driver Code Ends
